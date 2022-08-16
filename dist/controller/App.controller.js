6e994a52ff7a476c652c63a4b15482ef9842a876
lovywinsy <lovywinsy@gmail.com>
2022-08-16 22:17:42 +0800

    remove useless file and remove deps of package.json

--- dist/controller/App.controller.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(t){"use strict";return t.extend("sap.ui.demo.walkthrough.controller.App",{onInit:function(){this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())}})});
--- dist/test/unit/unitTests.qunit.js
+++
@@ -1,1 +0,0 @@
-QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["sap/ui/demo/walkthrough/test/unit/model/formatter"],function(){QUnit.start()})});
